bef33e8a96d143315ddb29090c044c3ff51f1752
chixx <lucottejeremie@gmail.com>
2024-10-30 22:30:29 +0100

    remove vehicle contracts

--- src/Cara7UpgredeableProxyVehicle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.25;
-
-import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
-
-contract Cara7UpgradeableProxyVehicle is UpgradeableBeacon {
-  constructor(address _implementation) UpgradeableBeacon(_implementation, msg.sender) {}
-}
