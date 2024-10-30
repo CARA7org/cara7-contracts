13b1b049ddf3a2809bdc50190700f5551d4db76e
chixx <lucottejeremie@gmail.com>
2024-10-30 22:29:42 +0100

    remove battery contract

--- src/Cara7UpgredeableProxyBattery.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.25;
-
-import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
-
-contract Cara7UpgradeableProxyBattery is UpgradeableBeacon {
-  constructor(address _implementation) UpgradeableBeacon(_implementation, msg.sender) {}
-}
